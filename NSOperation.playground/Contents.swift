84844ad48010eca86a94eb9358e963997b9a15d3
darya-varabei <82947997+darya-varabei@users.noreply.github.com>
2021-10-15 16:04:56 +0300

    add: example and comments

--- NSOperation.playground/Contents.swift
+++ NSOperation.playground/Contents.swift
@@ -1,44 +1,39 @@
 import UIKit
+import Foundation
 
 class RGBColors: NSObject {
-    
+
     static let redLight = "RED"
     static let greenLight = "GREEN"
     static let blueLight = "BLUE"
     func startSwitching() {
-        
-        let operationQueue = OperationQueue()
+
+        let operationQueue = OperationQueue() //create object of operationQueue class
         operationQueue.name = "Operation_queue"
-        
-        let redElements = BlockOperation {
+
+        let redElements = BlockOperation { //define variables of type BlockOperation with closures
             for _ in 0...10 {
                 print(RGBColors.redLight)
             }
         }
-        
+
         let greenElements = BlockOperation {
             for _ in 0...10 {
                 print(RGBColors.greenLight)
             }
         }
-        
-//        let blueElements = BlockOperation {
-//            for _ in 0...10 {
-//                print(RGBColors.blueLight)
-//            }
-//        }
-        
-        operationQueue.addOperation {
+
+       operationQueue.addOperation { //we can add operations to queue directly without defining objects themself
             for _ in 0...10 {
                 print(RGBColors.blueLight)
             }
         }
-        
+
         operationQueue.addOperation(redElements)
         operationQueue.addOperation(greenElements)
-        //operationQueue.addOperation(blueElements)
-        
-        operationQueue.waitUntilAllOperationsAreFinished()
+
+        operationQueue.waitUntilAllOperationsAreFinished() // wait for all operations to be finished to continue program execution
+        //as a result ooutput from operations is recieved in random order, as they are asynchronous and do not have dependencies
     }
 }
 
@@ -50,14 +45,14 @@ class Schedule {
         let operationQueue = OperationQueue()
         operationQueue.name = "morning_schedule"
         operationQueue.maxConcurrentOperationCount = 3
-        
+
         let bedroom = BlockOperation {
             print("Wake up")
             sleep(1)
             print("Make up bed")
             sleep(1)
         }
-        
+
         let bathroom = BlockOperation {
             print("Take bath")
             sleep(1)
@@ -66,7 +61,7 @@ class Schedule {
             print("Clean face")
             sleep(1)
         }
-        
+
         let kitchen = BlockOperation {
             print("Cook breakfast")
             sleep(1)
@@ -75,7 +70,7 @@ class Schedule {
             print("Drink coffee")
             sleep(1)
         }
-        
+
         let mobilePhone = BlockOperation {
             print("Check email")
             sleep(1)
@@ -84,13 +79,12 @@ class Schedule {
             print("Check telegram")
             sleep(1)
         }
-        
-        bathroom.addDependency(bedroom)
+
+        bathroom.addDependency(bedroom)// schedule tasks(bathroom starts execution only after bedroom is comleted etc)
         kitchen.addDependency(bathroom)
         mobilePhone.addDependency(bathroom)
-        
-        
-        operationQueue.addOperation(mobilePhone)
+
+        operationQueue.addOperation(mobilePhone)// add opeartions to queue
         operationQueue.addOperation(bedroom)
         operationQueue.addOperation(bathroom)
         operationQueue.addOperation(kitchen)
@@ -99,3 +93,28 @@ class Schedule {
 
 let schedule = Schedule()
 schedule.actions()
+
+let queue = OperationQueue()
+var workItem: BlockOperation?
+
+//we can check conditions of work items in thread
+print("Task is ready: ", workItem?.isReady ?? false)//checks if the task is comleted
+
+workItem = BlockOperation {
+    print("Task is in proccess: ", workItem!.isExecuting) // checks if task is currently in proccess
+    
+    var count = 0
+    while count < 100 {
+        count += 1
+    }
+}
+
+print("Task is ready: ", workItem!.isReady)
+
+workItem!.completionBlock = { // completion block is executed for queue items after their task is completed
+    print("Task is in process: ", workItem!.isExecuting)
+    print("Task is completed: ", workItem!.isFinished) // checks if task is not running any more
+}
+
+queue.addOperation(workItem!)
+
