a45739bfcb84c9fba5241dbbe9269e6d2b3f4ccf
darya-varabei <82947997+darya-varabei@users.noreply.github.com>
2021-10-18 17:48:55 +0300

    add: collection cell prototype and navigationController

---
+++ Cookbook/Cookbook/RecipeCell/RecipeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCollectionViewCell.swift
+//  Cookbook
+//
+//  Created by Darya on 10/18/21.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentView.layer.cornerRadius = 15
+        // Initialization code
+    }
+
+}
