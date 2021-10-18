c7a2e8c34a2d515b018c386ded2b4dc9a2c28cd5
darya-varabei <82947997+darya-varabei@users.noreply.github.com>
2021-10-18 18:00:44 +0300

    add: CookBookApi module

---
+++ CookBookApi/CookBookApi/CookBookApi.h
@@ -0,0 +1,18 @@
+//
+//  CookBookApi.h
+//  CookBookApi
+//
+//  Created by Darya on 10/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CookBookApi.
+FOUNDATION_EXPORT double CookBookApiVersionNumber;
+
+//! Project version string for CookBookApi.
+FOUNDATION_EXPORT const unsigned char CookBookApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CookBookApi/PublicHeader.h>
+
+
