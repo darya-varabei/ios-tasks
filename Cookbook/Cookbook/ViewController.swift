9e62c483760f5cc73de51367d765bb174cb95776
darya-varabei <82947997+darya-varabei@users.noreply.github.com>
2021-10-19 11:32:50 +0300

    add: network request

--- Cookbook/Cookbook/ViewController.swift
+++ Cookbook/Cookbook/ViewController.swift
@@ -6,11 +6,23 @@
 //
 
 import UIKit
+import CookBookApi
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak private var recipeCollection: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //let req = NetworkRequest()
+        let request = NetworkRequest()
+        request.fetchData { [weak self] (result: Result<Recipe,NetworkError>) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let recipe):
+                print(recipe)
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
