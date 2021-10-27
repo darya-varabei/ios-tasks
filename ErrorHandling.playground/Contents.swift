ee4059658079e7771a0fa7812a0b4dea3f58a44c
darya-varabei <82947997+darya-varabei@users.noreply.github.com>
2021-10-27 12:19:50 +0300

    add: second part and comments

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-enum CoffeeMachineError: Error {
+enum CoffeeMachineError: Error { //define enumeration of error situations
     case outOfBeans
     case outOfWater
     case outOfMilk
@@ -37,8 +37,8 @@ class CoffeeMachine {
         self.filterContaminationLevel = filterContaminationLevel
     }
     
-    func prepare(item: Item) throws {
-        guard item.beansNeededWeight <= self.beansWeight else {
+    func prepare(item: Item) throws { //function responsible for preparing beverages. But in some cases it may be impossible due to lack of water or beans in coffee machine, or milk container absence
+        guard item.beansNeededWeight <= self.beansWeight else { // guard statements enumerate situations  in which error throwing is requuired
             throw CoffeeMachineError.outOfBeans
         }
         
@@ -61,15 +61,15 @@ class CoffeeMachine {
         guard self.thickLevel < 20 else {
             throw CoffeeMachineError.cleanUpThick
         }
-        
+        //if no errors were thrown preparation is handled
         self.beansWeight -= item.beansNeededWeight
         self.waterWeight -= item.waterNeededWeight
         self.milkWeight -= item.milkNeededWeight
         self.filterContaminationLevel += item.filterContaminationLevel
-        self.thickLevel = item.thickProductionLevel
+        self.thickLevel += item.thickProductionLevel
         print("\(item.name) is prepared successfully")
     }
-    
+    // methods for coffee machine support
     func connectMilkContainer() {
         if !self.isMilkConnected {
             self.isMilkConnected = true
@@ -126,16 +126,65 @@ var items = [
     "Lungo": Item(name: "Lungo", beansNeededWeight: 30, isMilkNeeded: false, milkNeededWeight: 0, waterNeededWeight: 200, thickProductionLevel: 3, filterContaminationLevel: 3)
 ]
 
-func prepareCoffee(itemName: String, coffeeMachine: CoffeeMachine) throws {
+func prepareCoffee(itemName: String, coffeeMachine: CoffeeMachine) throws {// throwable function which finds out if such item is provided
     
     try coffeeMachine.prepare(item: (items[itemName] ?? Item(name: "Capuccino", beansNeededWeight: 10, isMilkNeeded: true, milkNeededWeight: 150, waterNeededWeight: 50, thickProductionLevel: 1, filterContaminationLevel: 2)))
 }
-let coffeeMachine = CoffeeMachine(beansWeight: 100, waterWeight: 500, isMilkConnected: false, milkWeight: 20, thickLevel: 0, filterContaminationLevel: 0)
+let coffeeMachine = CoffeeMachine(beansWeight: 100, waterWeight: 500, isMilkConnected: false, milkWeight: 20, thickLevel: 0, filterContaminationLevel: 0) // define coffee machine and its default state
 
-coffeeMachine.connectMilkContainer()
+coffeeMachine.connectMilkContainer()// connects milk container
 coffeeMachine.fillUpWithmilk()
 try prepareCoffee(itemName: "Latte", coffeeMachine: coffeeMachine)
 try prepareCoffee(itemName: "Espresso", coffeeMachine: coffeeMachine)
 try prepareCoffee(itemName: "Capuccino", coffeeMachine: coffeeMachine)
 coffeeMachine.fillUpWithmilk()
 try prepareCoffee(itemName: "Latte", coffeeMachine: coffeeMachine)
+//methods are placed in configuration which allows to escape errors throwing
+
+
+enum PasswordError: Error {
+    case invalid
+    case tooShort
+    case noDecimals
+}
+
+class User {
+    var username: String?
+    var password: String?
+    
+    init(username: String?, password: String?) throws { // initializers also can be throwable. In this case password is being validated
+        self.username = username
+        guard let password = password else {
+            throw PasswordError.invalid
+        }
+        guard password.count >= 15 else {
+            throw PasswordError.tooShort
+        }
+        
+        let decimals = CharacterSet.decimalDigits// checks if string contains digits
+        let decimalRange = password.rangeOfCharacter(from: decimals)
+        guard decimalRange != nil else {
+            throw PasswordError.noDecimals
+        }
+        self.password = password
+    }
+}
+
+// users definitions are wrapped into "do-catch satements" in order to produced errors would not prevent the code from further work, but printed out the definition of the error(default enumeration case string equals case name)
+do {
+    let _ = try User(username: "annyBunny", password: "eyrfb789euy97yh9898b")
+} catch let error {
+    print(error)
+}
+
+do {
+    let _ = try User(username: "amNickolsen", password: "asdfghjkloiuyuthrgegfhy")
+} catch let error {
+    print(error)
+}
+
+do {
+    let _ = try User(username: "samPeters", password: "1234fd")
+} catch let error {
+    print(error)
+}
