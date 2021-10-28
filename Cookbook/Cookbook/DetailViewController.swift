afbc2ee56085a4f23e2d3a4287e8a249039717ea
Darya Varabei <daria14072002@gmail.com>
2021-10-28 19:42:37 +0300

    fix: depreciated url session task

--- Cookbook/Cookbook/DetailViewController.swift
+++ Cookbook/Cookbook/DetailViewController.swift
@@ -127,10 +127,12 @@ extension UIImageView {
     public func imageFromUrl(urlString: String) {
         if let url = NSURL(string: urlString) {
             let request = NSURLRequest(url: url as URL)
-            NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: OperationQueue.main) {
-            (response: URLResponse?, data: Data?, error: Error?) -> Void in
+            let task = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
+                DispatchQueue.main.async {
                 self.image = UIImage(data: data! as Data)
+                }
             }
+            task.resume()
         }
     }
 }
