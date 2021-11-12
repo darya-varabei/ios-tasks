5bf6f25aa357055641514281bb6e55900c4998ad
darya-varabei <82947997+darya-varabei@users.noreply.github.com>
2021-11-12 12:38:52 +0300

    fix: delete unused system methods

---
+++ Priorities/Priorities/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Priorities
+//
+//  Created by Darya on 11/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+}
+
