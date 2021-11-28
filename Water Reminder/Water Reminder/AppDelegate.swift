2c803d9a3c26caedc531a390f0bf99a17a225e79
Darya Varabei <daria14072002@gmail.com>
2021-11-28 19:12:24 +0300

    add: send notification

--- Water Reminder/Water Reminder/AppDelegate.swift
+++ Water Reminder/Water Reminder/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let notificationCenter = NotificationManager()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         notificationCenter.requestAuthorization()
+        notificationCenter.sendNotifications()
         return true
     }
     
