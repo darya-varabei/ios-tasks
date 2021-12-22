965e5527db31dd0c2ec945dbc16da16350964ac4
darya-varabei <82947997+darya-varabei@users.noreply.github.com>
2021-12-22 13:37:37 +0300

    add: new person creation

--- TableViewApp/TableViewApp/Extensions/ViewController+AlertController.swift
+++ TableViewApp/TableViewApp/Extensions/ViewController+AlertController.swift
@@ -17,14 +17,24 @@ extension ViewController {
             preferredStyle: .alert
         )
         
-        alert.addAction(UIAlertAction(title: AlertParameters.addTitle, style: .cancel, handler: nil))
-        alert.addAction(UIAlertAction(title: AlertParameters.cancelTitle, style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: AlertParameters.cancelTitle, style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: AlertParameters.addTitle, style: .default, handler: { _ in
+            
+            guard let fields = alert.textFields else { return }
+            let newNameField = fields[0]
+            
+            guard let newName = newNameField.text, !newName.isEmpty else { return }
+            
+            self.namesArray.append(newName)
+            self.familyTableView.reloadData()
+        }))
         
         alert.addTextField { field in
             field.placeholder = AlertParameters.placeholder
             field.returnKeyType = .continue
         }
-        
+
         present(alert, animated: true)
     }
 }
+
