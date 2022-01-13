a2a09dfd22ac7a5e1a1d385c5165b002161b6d67
Darya Varabei <daria14072002@gmail.com>
2022-01-13 16:01:34 +0300

    add: model, view controller and view model for authorization page

---
+++ MVVMaApp/MVVMaApp/LoginManager.swift
@@ -0,0 +1,15 @@
+//
+//  LoginManager.swift
+//  MVVMaApp
+//
+//  Created by Дарья Воробей on 1/13/22.
+//
+
+import Foundation
+
+
+class LoginManager {
+    func loginWithCredentials(completionHandler: @escaping() -> Void){//(username: String, password: String) throws {
+        
+    }
+}
---
+++ MVVMaApp/MVVMaApp/ViewModel/LoginViewModel+LoginStatus.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewModel+LoginStatus.swift
+//  MVVMaApp
+//
+//  Created by Дарья Воробей on 1/13/22.
+//
+
+import Foundation
+
+extension LoginViewModel {
+    enum CredentialsInputStatus {
+        case Correct
+        case Incorrect
+    }
+}
