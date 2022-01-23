9856fd6a4d329aa22f99e942b66a4cfad09bc7c0
Darya Varabei <daria14072002@gmail.com>
2022-01-24 00:07:36 +0300

    add: coordinator on featured

--- MVVMaApp/MVVMApp/ViewController/FeaturedItems/FeaturedItemsViewController.swift
+++ MVVMaApp/MVVMApp/ViewController/FeaturedItems/FeaturedItemsViewController.swift
@@ -2,63 +2,61 @@
 //  FeaturedItemsViewController.swift
 //  MVVMApp
 //
-//  Created by Дарья Воробей on 1/15/22.
+//  Created by Дарья Воробей on 1/23/22.
 //
 
-import Foundation
 import UIKit
 
-class FeaturedItemsViewController: UIViewController, FeaturedBaseCoordinated {
+class FeaturedItemsViewController: UIViewController {
     
     @IBOutlet private var featuredTitleLabel: UILabel!
     @IBOutlet private var featuredCollectionView: UICollectionView!
-    
     var coordinator: FeaturedBaseCoordinator?
-    
+
     init(coordinator: FeaturedBaseCoordinator) {
         super.init(nibName: nil, bundle: nil)
         self.coordinator = coordinator
         title = "Favourite"
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     lazy var viewModel = {
         FeaturedBookViewModel()
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setDelegates()
         initViewModel()
     }
-    
+
     override func viewDidLayoutSubviews() {
         setupBackgroundColor()
         view.bringSubviewToFront(featuredCollectionView)
         view.bringSubviewToFront(featuredTitleLabel)
     }
-    
+
     private func setupBackgroundColor() {
         let gradient = CAGradientLayer()
-        
+
         guard let top = UIColor(named: Literals.gradientTopColor)?.cgColor else { return }
         guard let bottom = UIColor(named: Literals.gradientBottomColor)?.cgColor else { return }
         gradient.frame = view.bounds
         gradient.colors = [top, bottom]
-       
+
         view.layer.addSublayer(gradient)
     }
-    
+
     private func setDelegates() {
         featuredCollectionView.delegate = self
         featuredCollectionView.dataSource = self
         featuredCollectionView.backgroundColor = UIColor.clear
         featuredCollectionView.register(UINib(nibName: Literals.bookCell, bundle: nil), forCellWithReuseIdentifier: Literals.bookCell)
     }
-    
+
     private func initViewModel() {
         viewModel.getBooks()
         viewModel.reloadCollectionView = { [weak self] in
@@ -70,15 +68,15 @@ class FeaturedItemsViewController: UIViewController, FeaturedBaseCoordinated {
 }
 
 extension FeaturedItemsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.books.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: CellSizeProperties.width, height: CellSizeProperties.height)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Literals.bookCell, for: indexPath) as? BookCollectionViewCell else { fatalError(Literals.fatalErrorMessage) }
         let cellVM = viewModel.getCellViewModel(at: indexPath)
@@ -86,7 +84,7 @@ extension FeaturedItemsViewController: UICollectionViewDelegate, UICollectionVie
         cell.configure(viewModelGetObject: viewModel.getViewModel(index: indexPath.row))
         return cell
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if let viewController = storyboard?.instantiateViewController(identifier: Literals.detailViewController) as? DetailViewController {
             _ = viewController.view
