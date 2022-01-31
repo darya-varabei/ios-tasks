ea138eb97af402828c6832b1f7d81c0b08206997
Darya Varabei <daria14072002@gmail.com>
2022-02-01 00:47:40 +0300

    add: current workout view controller

---
+++ Workouts/Workouts/View Controllers/CurrentWorkoutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentWorkoutViewController.swift
+//  Workouts
+//
+//  Created by Дарья Воробей on 2/1/22.
+//
+
+import Foundation
+import UIKit
+
+class CurrentWorkoutViewController: UIViewController {
+    
+    @IBOutlet private var workoutCollectionView: UICollectionView!
+    @IBOutlet private var completeWorkoutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
