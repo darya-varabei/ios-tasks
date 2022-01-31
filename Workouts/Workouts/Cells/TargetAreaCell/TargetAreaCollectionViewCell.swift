e8358ec3392fbe913f5ce2dd6cf80a13f199d7a0
Darya Varabei <daria14072002@gmail.com>
2022-01-31 13:59:48 +0300

    add: target area cell setup

---
+++ Workouts/Workouts/Cells/TargetAreaCell/TargetAreaCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TargetAreaCollectionViewCell.swift
+//  Workouts
+//
+//  Created by Дарья Воробей on 1/31/22.
+//
+
+import UIKit
+
+class TargetAreaCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet private var targetAreaLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
