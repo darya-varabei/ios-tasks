b72fcd6523ad60130769551331b175a89ae06aad
Darya Varabei <daria14072002@gmail.com>
2022-02-01 00:09:25 +0300

    add: update exercise view controller setup

---
+++ Workouts/Workouts/Cells/ExerciseCell/ExerciseCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseCollectionViewCell.swift
+//  Workouts
+//
+//  Created by Дарья Воробей on 1/31/22.
+//
+
+import UIKit
+
+class ExerciseCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var exerciseParametersLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
---
+++ Workouts/Workouts/View Controllers/UpdateExerciseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateExerciseViewController.swift
+//  Workouts
+//
+//  Created by Дарья Воробей on 2/1/22.
+//
+
+import Foundation
+import UIKit
+
+class UpdateExerciseViewController: UIViewController {
+    
+    @IBOutlet private var exerciseNameTextField: UITextField!
+    @IBOutlet private var setsTextField: UITextField!
+    @IBOutlet private var repeatsTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
