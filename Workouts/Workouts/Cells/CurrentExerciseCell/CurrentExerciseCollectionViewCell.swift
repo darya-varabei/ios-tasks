83db5b97e57f2513eec39cf1d5e70dbbc1822267
Darya Varabei <daria14072002@gmail.com>
2022-02-01 01:01:42 +0300

    add: current cell setup

---
+++ Workouts/Workouts/Cells/CurrentExerciseCell/CurrentExerciseCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentExerciseCollectionViewCell.swift
+//  Workouts
+//
+//  Created by Дарья Воробей on 2/1/22.
+//
+
+import UIKit
+
+class CurrentExerciseCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var repeatsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
