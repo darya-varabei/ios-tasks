fd094036e412ec28ac9eea46fb6238ac27da61bb
Darya Varabei <daria14072002@gmail.com>
2022-01-31 11:27:15 +0300

    init: first setup

---
+++ Workouts/Workouts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Workouts
+//
+//  Created by Дарья Воробей on 1/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
