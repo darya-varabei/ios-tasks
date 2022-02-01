086a389c1155449dc77bdff95b723b7e9bb074d2
Darya Varabei <daria14072002@gmail.com>
2022-02-01 17:08:09 +0300

    refactor: exercise class

--- Workouts/Workouts/Model/Exercise.swift
+++ Workouts/Workouts/Model/Exercise.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-
 import CoreData
 
 final class Exercise: NSManagedObject {
@@ -15,4 +14,21 @@ final class Exercise: NSManagedObject {
     @NSManaged fileprivate(set) var repeats: Int16
     @NSManaged fileprivate(set) var sets: Int8
     @NSManaged fileprivate(set) var id: UUID
+    
+    static func findOrCreate(for idCode: UUID, in context: NSManagedObjectContext) -> Exercise {
+        let predicate = NSPredicate(format: "%K == %d", #keyPath(id), Int(idCode))
+        let country = findOrCreate(in: context, matching: predicate) {
+            $0.iso3166Code = isoCountry
+            $0.updatedAt = Date()
+            $0.continent = Continent.findOrCreateContinent(for: isoCountry, in: context)
+        }
+        return country
+    }
+
+    override func prepareForDeletion() {
+        guard let c = continent else { return }
+        if c.countries.filter({ !$0.isDeleted }).isEmpty {
+            managedObjectContext?.delete(c)
+        }
+    }
 }
