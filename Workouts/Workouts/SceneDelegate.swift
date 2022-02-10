289075a7ee5d53c35bd2ade2d7c3935cef7d18e7
Darya Varabei <daria14072002@gmail.com>
2022-02-10 11:03:10 +0300

    remove: unneeded variable

--- Workouts/Workouts/SceneDelegate.swift
+++ Workouts/Workouts/SceneDelegate.swift
@@ -11,15 +11,7 @@ import CoreData
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    
-    static var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "Model")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? { return }
-        })
-        return container
-    }()
-    
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         
