9863181a62e3c5f7c9dff673476004c5b7a78014
Darya Varabei <daria14072002@gmail.com>
2022-02-20 18:05:41 +0300

    init: primary setup

---
+++ ImageCollection/ImageCollection/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ImageCollection
+//
+//  Created by Дарья Воробей on 20.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private var imagesCollectionView: UICollectionView!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
