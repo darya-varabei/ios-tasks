697625dcf3f1062744ce169f7a9e55fe39431b0f
Darya Varabei <daria14072002@gmail.com>
2022-02-22 19:14:26 +0300

    add: core data inference with view model

--- ImageCoreData/ImageCollection/ViewModel/PhotoItemViewModel.swift
+++ ImageCoreData/ImageCollection/ViewModel/PhotoItemViewModel.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class PhotoItemViewModel {
     
     static private let numberOfImages = 30
     private let service = Service(numOfImages: numberOfImages)
+    private let coreDataService = CoreDataService()
     private let dispatchGroup = DispatchGroup()
+    private var fetchedResultsController: NSFetchedResultsController<Photo>?
     private var images = Observable<[UIImage]>()
     
     func loadImages() {
@@ -20,6 +23,15 @@ class PhotoItemViewModel {
         service.loadImages (completion: { [weak self] loadedImages in
             self?.images.value = loadedImages
         }, group: dispatchGroup)
+    }
+    
+    func loadFromStorage() {
+        fetchedResultsController = coreDataService.createRequest()
+        try? fetchedResultsController?.performFetch()
+    }
+    
+    func getObject(at indexPath: IndexPath) -> Photo? {
+        return fetchedResultsController?.object(at: indexPath)
     }
     
     func getImages() -> Observable<[UIImage]> {
